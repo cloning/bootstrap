3c979c3b475d052b4ffbe048972a1781e8cd94a6
Julian Duniec <julian.duniec@gmail.com>
2014-10-08 16:54:08 +0200

    Test refactoring

--- server/tests/app/api/api_test.go
+++ server/tests/app/api/api_test.go
@@ -2,47 +2,16 @@ package api_test
 
 import (
 	"../../../src/app/api"
-	"../../../src/app/services"
-	"fmt"
-	"io/ioutil"
-	"net/http"
 	"regexp"
-	"sync"
 	"testing"
 )
 
-const (
-	PORT = 4001
-)
-
-var apiUrl = fmt.Sprintf("http://localhost:%d", PORT)
-var service = services.NewService("Test")
-
 func TestJsonPShouldWrapResponse(t *testing.T) {
-	var wg sync.WaitGroup
+	withApi(func(api *api.Api) {
+		body, _, _ := get("/?jsonp=callback")
 
-	api := api.NewApi(service, PORT, wg)
-
-	go api.Run()
-
-	body, _, _ := get("/?jsonp=callback")
-
-	if matched, err := regexp.Match("callback(.*)", body); !matched || err != nil {
-		t.Errorf("Not wrapped in callback:%s", body)
-	}
-
-	api.Stop()
-	wg.Wait()
-}
-
-func get(path string) ([]byte, *http.Response, error) {
-	resp, err := http.Get(apiUrl + path)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-
-	return body, resp, err
+		if matched, err := regexp.Match("callback(.*)", body); !matched || err != nil {
+			t.Errorf("Not wrapped in callback:%s", body)
+		}
+	})
 }
